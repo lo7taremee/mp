0231698105352ef934a58878f87751f44fdc76ff
Mo7taref <41343228+Mo7taref@users.noreply.github.com>
2018-07-20 03:44:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
-
+client.on('message', message => {
+  if(message.content === ('clear')) {
+  let modRole = message.guild.roles.find("name", "Admin");
+  if (!modRole) return message.reply('You do not have Admin Role'); {
+    }
+  const params = message.content.split(" ").slice(1)
+    let messagecount = parseInt(params[0]);
+    message.channel.fetchMessages({limit: messagecount})
+        .then(messages => message.channel.bulkDelete(messages));
+  }
+});
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+      if(!message.member.hasPermission('ADMINISTRATOR'))
+        message.delete()
+    return message.reply(`** No Invite Links :angry: ! **`)
+    }
+});
+client.on('message', msg => {
+  if (msg.content === '$inv') {
+    msg.reply('https://discordapp.com/oauth2/authorize?=&client_id=468335086094254095&scope=bot&permissions=12659727');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === '$help') {
+    msg.reply(':envelope: | تم ارسال الرساله في الخاص');
+  }
+});
+client.on("message", message => {
+    var prefix = "$";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`$help `,"http://twitch.tv/S-F")
